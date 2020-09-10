389cb7e6b9ee7edc78f736b1944f25f102b684d0
qooqu <qooqu@users.noreply.github.com>
2020-09-10 10:50:17 +0100

    upgrade to 0.18.0

--- web/config/postcss.config.js
+++ web/config/postcss.config.js
@@ -1,19 +1,8 @@
 const path = require('path')
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    path.resolve(__dirname, '..', 'src', '**', '!(*.test).js'),
-    path.resolve(__dirname, '..', 'src', '**', '*.html'),
-  ],
-  // This extractor is used for tailwind classes.
-  // Read more here: https://tailwindcss.com/docs/controlling-file-size/
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = {
   plugins: [
-    require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
+    require('tailwindcss')(path.resolve(__dirname, './tailwind.config.js')),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
